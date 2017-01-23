e56b75bdca233a6ca2d4f80fad72abcc12692da4
Shelly Tang <shellytang@gmail.com>
2017-01-22 21:47:13 -0800

    updated comments

--- starter-code/scripts/controllers/articleController.js
+++ starter-code/scripts/controllers/articleController.js
@@ -14,6 +14,7 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+//This method calls on Article.findWhere that pulls from SQL the indicated field and value. Then in route.js, it uses page to set the path to /article/:id.
   articleController.loadById = function(ctx, next) {
     var articleData = function(article) {
       ctx.articles = article;
@@ -23,6 +24,7 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  // This method calls on Article.findWhere that pulls from SQL the field and value - for authorName it replaces the + with spaces. Then in route.js, it uses page to set the path to /author/:authorName.
   articleController.loadByAuthor = function(ctx, next) {
     var authorData = function(articlesByAuthor) {
       ctx.articles = articlesByAuthor;
@@ -35,6 +37,7 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+    // This method calls on Article.findWhere that pulls from SQL the field and value. Then in route.js, it uses page to set the path to /category/categoryName.
   articleController.loadByCategory = function(ctx, next) {
     var categoryData = function(articlesInCategory) {
       ctx.articles = articlesInCategory;
@@ -45,6 +48,7 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  // This method calls on Article.findWhere that pulls from SQL the field and value - it checks if all the article data is there, else it runs Article.fetchAll to get the data. Then in route.js, it uses page to set the path to /.
   articleController.loadAll = function(ctx, next) {
     var articleData = function(allArticles) {
       ctx.articles = Article.allArticles;
